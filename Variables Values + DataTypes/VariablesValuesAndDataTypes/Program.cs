458fb83abacb38ff69b8125257f04631d49d2bf4
ddbar <dbarbarie1@lycos.com>
2014-05-21 14:43:18 -0600

    created variables and played with operators

--- Variables Values + DataTypes/VariablesValuesAndDataTypes/Program.cs
+++ Variables Values + DataTypes/VariablesValuesAndDataTypes/Program.cs
@@ -52,6 +52,41 @@ namespace VariablesValuesAndDataTypes
 
             Console.WriteLine((int)result);
 
+            // variables of various data types, with values
+            string sentance = "a string can hold many characters.";
+            string empty = ""; // a string can be "empty" (zero characters)
+            string nothing = null; // a string can be null (not the same as "empty")
+
+            bool booleanVariable = true; // booleans can only hold values of true or false
+
+            bool bool1 = false;
+            
+            // please note that true is not the same as "true"
+            //                 \bool/                  \string/
+
+            // Variable names are decided upon by you when you declare the variable.
+            // Like everything else in c#, variable names are case sensitive..
+
+            int number = 25; // -> number assigned as 25
+            int Number = 15;
+            int NuMbER = 10;
+
+            // all of the above are case sensitive
+
+            // boolean expressions
+            
+            int highNumber = 100;
+            int lowNumber = 2;
+
+            bool highIsBiggerThanLow = highNumber > lowNumber;
+            //                         \relational expression/
+            //                          \   true or false   /
+            bool highIsBiggerThan50 = highNumber > 50;
+            bool highIs100 = highNumber == 100;
+
+            bool highIsBiggerThanLowAndBiggerThan50;
+            highIsBiggerThanLowAndBiggerThan50 = highIsBiggerThanLow && highIsBiggerThan50;
+
 
 
         }
