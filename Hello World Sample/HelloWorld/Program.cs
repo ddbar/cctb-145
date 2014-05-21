e9afe198c67f2f50f07057834c7fbd878354c910
ddbar <dbarbarie1@lycos.com>
2014-05-21 13:29:29 -0600

    Hello World Sample
    This is the intro sample we did in class.

---
+++ Hello World Sample/HelloWorld/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tell me your name:");
+            
+            // write line is a static method
+            // create some local variables
+
+            string name;
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+
+            int five = 5;
+            int seven = 7;
+
+            //initialization statements
+            
+            int total;
+            
+            // variable declaration statement
+
+            total = five + seven;
+            Console.WriteLine("Five + Seven is " + total);
+
+
+            double price = 10.95;
+            int quantity = 3;
+
+            double totalPrice;
+            totalPrice = price * quantity;
+
+            // assignment statement
+
+            Console.Write(price);
+            Console.Write(" times");
+            Console.Write(quantity);
+            Console.Write(" equals ");
+            Console.WriteLine(totalPrice);
+            Console.WriteLine("Computers do math...");
+
+        }
+    }
+}
